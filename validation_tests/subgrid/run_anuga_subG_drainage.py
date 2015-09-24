91709bb31c8e83fcfdb3d2e3eba71a813df543ca
benjimin <ben.lewis@ga.gov.au>
2015-09-24 14:20:22 +1000

    errors..

--- validation_tests/subgrid/run_anuga_subG_drainage.py
+++ validation_tests/subgrid/run_anuga_subG_drainage.py
@@ -1,6 +1,10 @@
 """
 
-Very shallow water flowing down a coarsely resolved slope, with drains
+Very shallow water flowing down a coarsely resolved slope
+
+Many algorithms have trouble with this when the elevation range of each cell
+is much larger than the centroid water depth. But it is an important case to
+get basically correct.
 
 """
 
@@ -16,6 +20,10 @@ from anuga import create_domain_from_regions
 # Useful parameters for controlling this case
 #------------------------------------------------------------------------------
 
+stage_rel_to_plain = -0.05 # Target steady state analytic water depth
+drain_depth = -1
+filename='drainage'
+
 floodplain_length = 1000.0 # Model domain length
 floodplain_width = 100.0 # Model domain width
 floodplain_slope = 1./10.
@@ -23,38 +31,34 @@ chan_initial_depth = 0.0 # Initial depth of water in the channel
 chan_bankfull_depth = 0.0 # Bankfull depth of the channel
 chan_width = 5. # Bankfull width of the channel
 bankwidth = 2. # Width of the bank regions -- note that these protrude into the channel
-man_n = 0.03 # Manning's n
-l0 = 10 # Length scale associated with triangle side length in channel (min_triangle area = 0.5*l0^2)
-#l0 = 4
-
-filename='drainage-flat'
+man_n=0.03 # Manning's n
+l0 = 15 # Length scale associated with triangle side length in channel (min_triangle area = 0.5*l0^2)
+#l0 = 2
 
 flow_in_yval = 5.0 # y-value of line along which the input discharge is passed
 #Qin = 0.5 # Input discharge
 
-stage_rel_to_plain = 0.3 # Target steady state analytic water depth
-
-flow_algorithm = 'DE_SG' #'DE1' #'DE_SG'
-reference_gradient_type = 'bed-slope' #'zero' #'bed-slope'
+flow_algorithm = 'DE_SG'
+reference_gradient_type = 'bed-slope'
 
 drain_count = 3
 drains_proportion_of_width = 0.25
 drains_proportion_of_length = 0.8
-drain_depth = -3.0 #-0.000001
+
 
 """
 We know Q = sqrt(abs(slope))/n_manning * integral of depth^(5/3) across transverse axis
 And since the integrand is piecewise constant...
 """
-assert stage_rel_to_plain > drain_depth
-drain_portion = drains_proportion_of_width * (stage_rel_to_plain - drain_depth)**(5.0/3.0)
+#assert stage_rel_to_plain > drain_depth
+drain_portion = drains_proportion_of_width * max(0.0,stage_rel_to_plain-drain_depth)**(5.0/3.0)
 plain_portion = (1.0-drains_proportion_of_width) * max(0.0,stage_rel_to_plain)**(5.0/3.0)
 Qin = abs(floodplain_slope)**(0.5)/man_n * floodplain_width*(drain_portion + plain_portion)
-#assert Qin > 0
+assert Qin > 0
 
-assert int(drain_count) >= 1
-assert 0 < drains_proportion_of_width < 1
-assert 0 < drains_proportion_of_length < 1
+assert int(drain_count)>=1
+assert 0<drains_proportion_of_width<1
+assert 0<drains_proportion_of_length<1
 
 assert chan_width < floodplain_width, \
         ' ERROR: Channel width is greater than floodplain width'
@@ -63,11 +67,10 @@ assert bankwidth < chan_width/2., \
 	'ERROR: The bank width must be less than half the channel width'
 
 def topography(x,y):
-    elev = -y*floodplain_slope
-
+    elev=-y*floodplain_slope
     # modification to cut some drains into the bed
     drain_width = (floodplain_width/drain_count)
-    padding = drain_width * 0.5 * (1-drains_proportion_of_width)
+    padding = drain_width * 0.5*(1-drains_proportion_of_width)
     x = x % drain_width
     """
     if float(y)/floodplain_length > (1-drains_proportion_of_length):
@@ -81,12 +84,11 @@ def topography(x,y):
     return elev
 
 # Very low friction [frictionless would be ideal for testing, but can't be done]
-# (Although if it were frictionless, there could not be steady-uniform flow)
 def frictionFun(x,y):
     return man_n + x*0.
 
 def stageFun(x,y):
-    out = topography(x,y) -100.0 # Dry
+    out = topography(x,y)-100. # Dry
     return out
 
 #------------------------------------------------------------------------------
@@ -159,19 +161,18 @@ if __name__ == '__main__':
 
     domain.quantities_to_be_stored['elevation'] = 2
 
-    domain.evolve_max_timestep = 1.0
     #from anuga.utilities import subGridUtil as sgu
     #mean_topography=sgu.make_spatially_averaged_Fun(
     #    topography, domain, approx_grid_spacing = [0.5, 0.5])
     #domain.set_quantity('elevation', mean_topography, location='centroids')
-    domain.set_quantity('elevation', topography, location='centroids') #, location='centroids')
-    domain.set_quantity('stage', stageFun, location='centroids')
+    domain.set_quantity('elevation', topography) #, location='centroids')
+    domain.set_quantity('stage', stageFun)
     domain.set_quantity('xmomentum', 0.)
     domain.set_quantity('ymomentum', 0.)
     domain.set_quantity('friction', frictionFun)
     #domain.set_quantity('height',0.1)
 
-    if(domain.flow_algorithm == 'DE_SG'):
+    if(domain.flow_algorithm=='DE_SG'):
         domain.set_quantity('alphax',0.)
         domain.set_quantity('alphay',0.)
 
@@ -179,9 +180,9 @@ if __name__ == '__main__':
         domain.subgrid_data.make_subgrid_tables(
             topography, frictionFun, 
             approx_grid_spacing=[5.0,5.0], 
-            max_reference_depth=20., 
+            max_reference_depth=10., 
             constant_subgrid_values=False,
-            reference_gradient_type=reference_gradient_type) 
+            reference_gradient_type='bed-slope') 
             #reference_gradient_type='zero')        
 
         # Set initial values for volume quantities
@@ -221,18 +222,11 @@ if __name__ == '__main__':
 
     #domain.CFL=0.1
 
-    # GD NOTE: Might be too large a timestep on the first step?
-    # No -- but it rapidly becomes unstable later on
-    # domain.evolve_max_timestep = 0.01
-    for t in domain.evolve(yieldstep=5.0, finaltime=100*60.0):
-
-        print domain.time, domain.timestep, domain.edge_timestep.min()
-
+    for t in domain.evolve(yieldstep=5.0,finaltime=100*60.0):
+        print domain.time, domain.edge_timestep.min()
         domain.report_water_volume_statistics()
-
         #domain.report_cells_with_small_local_timestep()
         if domain.flow_algorithm == "DE_SG":
-            print 'Volume: ', domain.quantities['vol'].centroid_values.sum()
             vv = domain.quantities['u_vol'].centroid_values**2+domain.quantities['v_vol'].centroid_values**2
             velsq = (vv/(domain.quantities['vol'].centroid_values**2+1.0e-10))
             print 'vel_max: ', (velsq.max())**0.5
